e2ac0cab803879dd7efb635a1b290153b46e6b53
Vito Di Grigoli <73437231+vitodigrigoli@users.noreply.github.com>
2023-09-19 13:25:44 +0200

    add translations

---
+++ translations.py
@@ -0,0 +1,29 @@
+it_strings = {
+    "welcome": "Ciao{username}! Sono il tuo nuovo bot personale 🫡",
+    "choose_language": "Per favore seleziona la tua lingua",
+    "selected_language": "La lingua è stata impostata correttamente ✅",
+    "choose_username": "Come preferisci essere chiamato?",
+    "instruction": "Per favore, scegli un'opzione.",
+    "commands": {
+        "start": "Inizia ad usare il bot",
+        "help": "Ottieni informazioni su come usare il bot",
+        # ... (aggiungi altre descrizioni di comandi qui)
+    }
+}
+
+
+
+
+
+en_strings = {
+    "welcome": "Hi{username}! I'm your new personal bot 🫡",
+    "choose_language": "Please select your language",
+    "selected_language": "The language has been selected correctly ✅",
+    "choose_username": "What do you prefer to be called?",
+    "instruction": "Please choose an option.",
+    "commands": {
+        "start": "Start using the bot",
+        "help": "Get information on how to use the bot",
+        # ... (add other command descriptions here)
+    }
+}
